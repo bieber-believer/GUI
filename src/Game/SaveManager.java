c092c3725aa4e7798df4025a39175e8d3504dde5
acyviesca <amber_viesca@dlsu.edu.ph>
2026-08-03 06:05:03 +0800

    Rename SAVE_FILE to FILE_NAME in SaveManager

--- src/Game/SaveManager.java
+++ src/Game/SaveManager.java
@@ -2,18 +2,18 @@ package Game;
 import java.io.*;
 
 public class SaveManager {
-    private static final String SAVE_FILE = "save.txt";
+    private static final String FILE_NAME = "save.txt";
 
-    public static PrintWriter createWriter() throws IOException{
-        return new PrintWriter(new FileWriter(SAVE_FILE));
+    public static PrintWriter getWriter() throws IOException{
+        return new PrintWriter(new FileWriter(FILE_NAME));
     }
 
-    public static BufferedReader createReader() throws IOException{
-        return new BufferedReader(new FileReader(SAVE_FILE));
+    public static BufferedReader getReader() throws IOException{
+        return new BufferedReader(new FileReader(FILE_NAME));
     }
 
     public static boolean saveExists(){
-        File file = new File(SAVE_FILE);
+        File file = new File(FILE_NAME);
         return file.exists();
     }
 }
