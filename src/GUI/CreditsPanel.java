20c70a51a68c80adf79cb22fa2b0e6688337fd78
ape ish <dyjasyza@gmail.com>
2026-08-03 06:14:15 +0800

    add more credits

--- src/GUI/CreditsPanel.java
+++ src/GUI/CreditsPanel.java
@@ -20,9 +20,13 @@ public class CreditsPanel extends JPanel {
         // TODO: replace with your real sprite sources + developer names
         String creditsText =
                 "Sprites & Assets:\n\n" +
-                        "- Tile sprites: [Artist Name] (Year). [Title]. [Publisher/Site]. [Link]\n" +
-                        "- Player sprite: [Artist Name] (Year). [Title]. [Publisher/Site]. [Link]\n" +
-                        "- Bat sprite: [Artist Name] (Year). [Title]. [Publisher/Site]. [Link]\n\n" +
+                        "- Tile sprites: https://morain.itch.io/backgrounds-and-textures\n" +
+                        "- Gold sprites: https://free-game-assets.itch.io/free-goblin-loot-icons-3232-pixel-art\n" +
+                        "- Treasure sprites: https://schwarnhild.itch.io/basic-caves-and-dungeons-tileset-32x32-pixels\n" +
+                        "- Lailaps sprite: https://netherzapdos.itch.io/paws-whiskers-isometric-dogs-pack\n" +
+                        "- Player sprite:https://toffeecraft.itch.io/bunny-character-pixel\n" +
+                        "- Siren sprite:https://maranza.itch.io/assets?download\n" +
+                        "- Bat sprite: https://papoycore.itch.io/bat\n\n" +
                         "Developed by:\n" +
                         "- Jasmine Yzabelle Y. Dy\n" +
                         "- Amber Chardonnay Viesca";
